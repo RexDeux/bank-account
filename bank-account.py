c5a8b8defa2d9ac20fafe21632cf9d3599496311
RexDeux <nm.cdc@hotmail.com>
2022-02-10 18:26:15 +0000

    fixed methods after testing

--- bank-account.py
+++ bank-account.py
@@ -3,23 +3,24 @@ class Account:
         self.title = title
         self.balance = balance
         pass
-    def getBalance(self, title, balance):
-        super().__init__(title, balance)
-        return balance
-    def deposit(self, balance,ammount):
-        self.ammount = ammount
-        ammount += balance
-    def withdrawl(self, balance, ammount):
-        self.ammount = ammount
-        ammount -= balance
+
+    def getBalance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+
+    def withdrawl(self, amount):
+        self.balance = self.balance - amount
+
+        
 class SavingsAccount(Account):
     def __init__(self, title=None, balance=None, interestRate= 0):
         super().__init__(title, balance)
         self.interestRate = interestRate
         pass
+
     def interestAmmount(self, balance, interestRate, interest_ammount):
-        super().__init__(interestRate, balance)
-        self.interest_ammount = interest_ammount
-        interest_ammount = (self.interestRate * self.balance) / 100
+        return(self.interestRate * self.balance) / 100
 
 demo1 = SavingsAccount("Mark", 2000, 5)
