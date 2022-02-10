bb6171e44aba3864b7022009a76eb026f378d1d3
RexDeux <nm.cdc@hotmail.com>
2022-02-10 18:20:58 +0000

    more classes bby

--- bank-account.py
+++ bank-account.py
@@ -3,12 +3,23 @@ class Account:
         self.title = title
         self.balance = balance
         pass
-
+    def getBalance(self, title, balance):
+        super().__init__(title, balance)
+        return balance
+    def deposit(self, balance,ammount):
+        self.ammount = ammount
+        ammount += balance
+    def withdrawl(self, balance, ammount):
+        self.ammount = ammount
+        ammount -= balance
 class SavingsAccount(Account):
-   def __init__(self, title=None, balance=None, interestRate= 0):
+    def __init__(self, title=None, balance=None, interestRate= 0):
         super().__init__(title, balance)
         self.interestRate = interestRate
         pass
+    def interestAmmount(self, balance, interestRate, interest_ammount):
+        super().__init__(interestRate, balance)
+        self.interest_ammount = interest_ammount
+        interest_ammount = (self.interestRate * self.balance) / 100
 
-#A = SavingsAccount()
-#A("Nuno", 10000, 5)
+demo1 = SavingsAccount("Mark", 2000, 5)
