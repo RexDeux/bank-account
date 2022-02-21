e5a16a726ebaaeaa1a9859c7b7ff8701ca65a0d7
RexDeux <nm.cdc@hotmail.com>
2022-02-21 17:44:59 +0000

    fixed exploit

--- twitter.py
+++ twitter.py
@@ -6,14 +6,15 @@ import json
 import config
 from twython import Twython, TwythonError
 
-class Twitter(request):
+def Twitter(request):
+    coin = Twython(config.secret_api_key, config.secret_api_secret, config.secret_access_token, config.secret_token_secret)
     url = 'https://serpapi.com/search.json?q=Naval+twitter&location=Austin%2CTexas%2CUnited+States&hl=en&gl=us&api_key={secret_api_key}'
     params = {
     "q": "Naval twitter",
     "location": "Austin,Texas,United States",
     "hl": "en",
     "gl": "us",
-    "api_key": "secret_api_key"
+    "secret_api_key": config.secret_api_key,
     }
 
     search = GoogleSearch(params)
